0bce7e2ee32560105c1c06cab58b8c6784e36ef2
viktor <victorhbenitez85@hotmail.com>
2020-03-12 11:06:05 -0600

    Add playGround description

---
+++ MVPExample/Description.playground/Pages/MVP.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+/*:
+# MVP ARCHITECTURE
+_ _ _ _ _ _ _ _ _
+
+![MPV](MPV.png)
+
+
+The component description becomes at the following:
+
+View:
+The view consists views and ViewControllers, with all UI setup and event
+view will only passe actions and listen to the presenter updates.
+
+Presenter:
+- It will be in change of all the logic
+- Responding to user actions and updating the UI (DELEGATE)
+- Presenter will not be UIKit dependent
+
+Presente will trigger the ui updates using delegates.
+
+Model: The model role will be exactly the same
+
+//: Find more information for [Medium MVP pattern](https://medium.com/@saad.eloulladi/ios-swift-mvp-architecture-pattern-a2b0c2d310a3)
+ 
+ - [Example](Example)
+ 
+*/
